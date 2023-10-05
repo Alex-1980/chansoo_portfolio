27bcc4e765e10f50b802779f9ecbc11ffea470cb
Chansoo <devhan1207@gmail.com>
2023-10-05 14:32:17 -0400

    contact page done

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,35 @@
 import Loader from 'react-loaders';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
+  const refForm = useRef();
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover');
     }, 3000)
   }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm('gmail', 'template_m05fvan', refForm.current, 'jHoNVxpxRrhufonUb')
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
 
   return (
     <>
@@ -29,7 +48,7 @@ const Contact = () => {
             questions, don't hesitate to contact me using below form either.
           </p>
           <div className='contact-form'>
-            <form>
+            <form ref={refForm} onSubmit={sendEmail}>
               <ul>
                 <li className='half'>
                   <input type='text' name='name' placeholder='Name' required />
@@ -49,6 +68,20 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className='info-map'>
+          Chansoo Han <br />
+          1535 Trossacks Ave. <br />
+          London, ON, Canada <br />
+          <span>devhan1207@gmail.com</span>
+        </div>
+        <div className='map-wrap'>
+          <MapContainer center={[43.03853113769225, -81.24607428569928]} zoom={13}>
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            <Marker position={[43.03853113769225, -81.24607428569928]}>
+              <Popup>Chansoo lives here, come over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type='pacman' />
