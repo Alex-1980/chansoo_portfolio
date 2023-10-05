afd511c60ba0166ee789b5e6f470468358030ee8
Chansoo <devhan1207@gmail.com>
2023-10-05 10:51:47 -0400

    about page done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import './index.scss';
 import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from './Logo';
+import Loader from 'react-loaders';
 
 export default function Home() {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -11,30 +12,33 @@ export default function Home() {
 
   useEffect(() => {
     setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 4000)
-  },[])
+      setLetterClass('text-animate-hover');
+    }, 4000);
+  }, []);
 
   return (
-    <div className='container home-page'>
-      <div className='text-zone'>
-        <h1>
-        <span className={letterClass}>H</span>  
-        <span className={`${letterClass} _12`}>i,</span>  
-        <br />
-        <span className={`${letterClass} _12`}>I</span>
-        <span className={`${letterClass} _12`}>'m&nbsp;</span>
-        <AnimatedLetters letterClass={letterClass}
-        strArray={nameArray} idx={15} />
-          <br />
-          <AnimatedLetters letterClass={letterClass}
-        strArray={jobArray} idx={22} />
-        </h1>
-        <h2>Full-stack Developer</h2>
-        <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+    <>
+      <div className='container home-page'>
+        <div className='text-zone'>
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _12`}>I</span>
+            <span className={`${letterClass} _12`}>'m&nbsp;</span>
+            <AnimatedLetters letterClass={letterClass}
+              strArray={nameArray} idx={15} />
+            <br />
+            <AnimatedLetters letterClass={letterClass}
+              strArray={jobArray} idx={22} />
+          </h1>
+          <h2>Full-stack Developer</h2>
+          <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type='pacman' />
+    </>
   );
 }
 
